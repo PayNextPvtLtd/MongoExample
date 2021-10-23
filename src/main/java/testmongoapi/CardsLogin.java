3436b5a576212d4baf4c7564eb138803fd771571
Abhishek M <abhishek.m@paynext.co.in>
2021-10-23 07:38:53 +0530

    Summary
    Return JSON

--- src/main/java/testmongoapi/CardsLogin.java
+++ src/main/java/testmongoapi/CardsLogin.java
@@ -13,11 +13,11 @@ public class CardsLogin
 
     private String username;
     private String password;
-    private int role;
-    private boolean active;
+    private Integer role;
+    private Boolean active;
     private JSONObject riskprofile;
 
-    public CardsLogin(String username, String password, int role, boolean active, JSONObject riskprofile)
+    public CardsLogin(String username, String password, Integer role, Boolean active, JSONObject riskprofile)
     {
         super();
 
@@ -25,6 +25,56 @@ public class CardsLogin
         this.password = password;
         this.role = role;
         this.active = active;
+        this.riskprofile = riskprofile;
+    }
+
+    public String getUsername()
+    {
+        return username;
+    }
+
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public Integer getRole()
+    {
+        return role;
+    }
+
+    public void setRole(Integer role)
+    {
+        this.role = role;
+    }
+
+    public Boolean getActive()
+    {
+        return active;
+    }
+
+    public void setActive(Boolean active)
+    {
+        this.active = active;
+    }
+
+    public JSONObject getRiskprofile()
+    {
+        return riskprofile;
+    }
+
+    public void setRiskprofile(JSONObject riskprofile)
+    {
         this.riskprofile = riskprofile;
     }
 
--- src/main/java/testmongoapi/Controller.java
+++ src/main/java/testmongoapi/Controller.java
@@ -1,11 +1,15 @@
 package testmongoapi;
 
-import java.util.concurrent.CompletableFuture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 /**
  *
@@ -18,6 +22,10 @@ public class Controller
     @Autowired
     ICardsLogin cardsLoginRepo;
 
+    /**
+     *
+     * @return
+     */
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home()
     {
@@ -28,18 +36,46 @@ public class Controller
         return rsp;
     }
 
+    /**
+     *
+     * @param request
+     * @return
+     */
     @RequestMapping(value = "/addrecord", method = RequestMethod.POST, headers = "Accept=application/json")
     public String addrecord(@RequestBody APICardsLogin request)
     {
         String rsp;
-        
+
         cardsLoginRepo.save(new CardsLogin(request.getUsername(), request.getPassword(), request.getRole(), true, request.getRiskprofile()));
 
-        rsp = "Done.\n";
+        rsp = String.format("Total records = %d\n", cardsLoginRepo.count());
 
         return rsp;
     }
 
+    /**
+     * 
+     * @param username
+     * @return 
+     */
+    @RequestMapping(value = "/findbyuser", method = RequestMethod.GET)
+    public ResponseEntity<CardsLogin> findbyuser(@RequestParam String username)
+    {
+        String rsp;
+        Gson clgson = new Gson();
+
+        CardsLogin cl = cardsLoginRepo.findItemByName(username);
+        
+        rsp = clgson.toJson(cl);
+
+        return new ResponseEntity<>(cl, HttpStatus.OK);
+    }
+
+    /**
+     *
+     * @param request
+     * @return
+     */
     @RequestMapping(value = "/transaction", headers = "Accept=application/json", method = RequestMethod.POST)
     public String transaction(@RequestBody String request)
     {
